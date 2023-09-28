7ad895ad77abbe4b13bb72dea600089722a58bf8
Ameer <ak2260650@gmail.com>
2023-09-28 18:37:08 +0530

    .

--- client/store/slices/dataSlice.ts
+++ client/store/slices/dataSlice.ts
@@ -2,10 +2,12 @@ import { StateCreator } from "zustand";
 
 import { io, Socket } from "socket.io-client";
 
-let URL = process.env.NEXT_PUBLIC_BACKEND_URL!;
+let URL: string;
 
 if (process.env.NODE_ENV !== "production") {
-  URL = "http://127.0.0.1:5000";
+  URL = "http://127.0.0.1:5000/api/v1";
+} else {
+  URL = `${process.env.BACKEND_URL!}/api/v1`;
 }
 
 export interface DataSlice {
